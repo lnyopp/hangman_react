2f690e2555fc94c6a58259b68735ac87eff1588d
Rumble Roar <lnyopp15@gmail.com>
2020-08-11 21:26:23 -0400

    practice react with hooks, through traversy media

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState,useEffect }from 'react';
 import './App.css';
+import Header from "./components/Header";
+import Figure from "./components/Figure";
+import WrongLetters from "./components/WrongLetters";
+import Word from "./components/Word"
+import PopUp from './components/PopUp';
+
+
+const words = ["application", "programming", "interface", "wizard"];
+
+let selectedWord = words[Math.floor(Math.random() * words.length)];
+
 
 function App() {
+
+  let [playable,setPlayable] = useState(true);
+  let[correctLetters, setCorrectLetters] = useState([])
+  let [wrongLetters, setWrongLetters] = useState([]);
+
+
+  useEffect(()=>{
+      let handlekeyDown = event =>{
+        let {key, keyCode} = event
+
+      if (playable && (keyCode >= 65 && keyCode <= 90)){
+        let letter = key.toLowerCase();
+
+        if (selectedWord.includes(letter)) {
+          if (!correctLetters.includes(letter)) {
+            setCorrectLetters = (currentLetters => [...currentLetters, letter]);
+          } else {
+            // showNotification();
+          }
+        } else {
+          if (!wrongLetters.includes(letter)) {
+           setWrongLetters = (wrongLetters => [...wrongLetters, letter]);
+
+  
+          } else {
+            // showNotification();
+          }
+        }
+      }
+    }
+
+      window.addEventListener("keydown",handlekeyDown);
+
+      return () => window.removeEventListener("keydown", handlekeyDown);
+
+  }, [correctLetters,wrongLetters,playable]);
+
+
+
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <div className="game-container">
+        <Figure />
+        <WrongLetters wrongLetters ={wrongLetters} />
+        <Word selectedWord ={selectedWord} correctLetters={correctLetters}/>
+      </div>
+
+      <div class="popup-container" id="popup-container">
+        <PopUp />
+      </div>
+    </>
   );
 }
 
---
+++ src/components/PopUp.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const PopUp = () => {
+    return (
+      <div class="popup">
+        <h2 id="final-message"></h2>
+        <h3 id="final-message-reveal-word"></h3>
+        <button id="play-button">Play Again</button>
+      </div>
+    );
+}
+
+export default PopUp;
